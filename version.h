40c22eb2435a628fd59cdc4109ca09825c580810
angel¿ <155915830+NopAngel@users.noreply.github.com>
2026-05-17 18:00:29 -0400

    `version.h: fucking shit`

---
+++ version.h
@@ -0,0 +1,16 @@
+#ifndef __VERSION_H
+#define __VERSION_H
+
+#define VERSION_MAJOR 1
+#define VERSION_MINOR 0
+#define VERSION_BUILD 0
+
+#define _NAME "uClite"
+#define _NAME_BETA_ "ucl"
+
+#define _STR(x) #x
+#define STR(x) _STR(x)
+#define VERSION_STR STR(VERSION_MAJOR) "." STR(VERSION_MINOR) "." STR(VERSION_BUILD)
+#define VERSION_NUM VERSION_MAJOR, VERSION_MINOR, VERSION_BUILD
+
+#endif
