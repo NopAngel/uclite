97cd8f5191cc3a30bde73ce436431dd11a324b06
angel¿ <155915830+NopAngel@users.noreply.github.com>
2026-05-19 15:25:22 -0400

    Bump version: v1.1.0 - `version.h`

--- version.h
+++ version.h
@@ -2,23 +2,22 @@
 #define __VERSION_H
 
 
-
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 1
 #define VERSION_BUILD 0
-
 
 
 #define _NAME "uClite"
 #define _NAME_BETA_ "ucl_c"
 
 
+#define RELEASE "development"
+
 
 #define _STR(x) #x
 #define STR(x) _STR(x)
 #define VERSION_STR STR(VERSION_MAJOR) "." STR(VERSION_MINOR) "." STR(VERSION_BUILD)
 #define VERSION_NUM VERSION_MAJOR, VERSION_MINOR, VERSION_BUILD
-
 
 
 #endif
