8bf682cfad32d484a301fca4f96f734606ae8776
Angel Nieto <angelgabrielnieto@outlook.com>
2026-07-06 00:34:52 -0400

    uclite: exit.c

--- src/exit/exit.c
+++ src/exit/exit.c
@@ -1,17 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-
 extern FILE *stdout;
 extern FILE *stderr;
 int fflush(FILE *stream);
 void _exit(int status);
 
-void exit(int status) {
-    
+void exit(int status) 
+{    
     if (stdout) fflush(stdout);
     if (stderr) fflush(stderr);
 
-    
     _exit(status);
 }
